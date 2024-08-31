0b7a4f059639d6b0cdb1c41f2c3d703e8d42f428
kaikmarques.ti@hotmail.com <kaikmarques.ti@hotmail.com>
2024-08-31 17:56:15 -0300

    add exercicio CalculadoraRetangulo

---
+++ Exercicios/FormulasMatematicas/Exercicios/FormulasMatematicas/Exercicios/FormulasMatematicas/CalculadoraRetangulo.java
@@ -0,0 +1,29 @@
+package Exercicios.FormulasMatematicas;
+
+/* Escreva um programa que calcule o perímetro e a área de um retângulo, 
+ * utilizando as fórmulas P = 2(l + c) e A = lc, onde l é a largura e c é o comprimento
+ * 
+ * 
+ *  1. Calcular o Perimetro(P)
+ * Fórmula:
+ *  A fórmula para o perímetro é P=2(l+c)
+ * 
+ * Some a largura l e o comprimento 𝑐
+ * Multiplique o resultado por 2.
+ * 
+ * Exemplo: Se a largura l é 5 metros e o comprimento c é 10 metros: 𝑃 = 2 ( 5 + 10 ) = 2 ( 15 ) = 30 metros P=2(5+10)=2(15)=30 metros
+ * 
+ *  2. Calcular a Area(A)
+ * 
+ * A fórmula para a área é: A = lc
+ * Multiplique a largura l pelo comprimento c.
+ * 
+ * Exemplo: Se a largura l é 5 metros e o comprimento c é 10 metros: 𝐴 = 5 × 10 = 50 metros quadrados A=5×10=50 metros quadrados
+ * 
+ * 
+ * 
+ */
+
+public class CalculadoraRetangulo {
+
+}
